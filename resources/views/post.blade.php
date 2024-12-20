62efb8511a2f7aef4a6188fc2c392ea75e7b848a
Rinaldi Prasya <103562307+rprasya@users.noreply.github.com>
2024-12-20 12:27:21 +0700

    feat: feat: add search feature to blog page

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,22 +1,5 @@
 <x-layout>
     <x-slot:title>{{ $title }}</x-slot:title>
-
-
-    {{-- <article class="py-8 max-w-screen-md">
-        <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
-        <div>
-            By
-            <a href="/authors/{{ $post->author->username }}"
-                class="text-base text-gray-500 hover:underline">{{ $post->author->name }}</a>
-            in
-            <a href="/categories/{{ $post->category->slug }}"
-                class="text-base text-gray-500 hover:underline">{{ $post->category->name }}</a>
-            | {{ $post['created_at']->diffForHumans() }}
-        </div>
-        <p class="my-4 font-light">{{ $post['body'] }}</p>
-        <a href="/posts" class="font-medium text-blue-500 hover:underline">&laquo; Back to posts</a>
-    </article> --}}
-
     <main class="pt-8 pb-16 lg:pt-16 lg:pb-24 bg-white dark:bg-gray-900 antialiased">
         <div class="flex justify-between px-4 mx-auto max-w-screen-xl ">
             <article
@@ -30,13 +13,13 @@
                                 src="https://flowbite.com/docs/images/people/profile-picture-2.jpg"
                                 alt="{{ $post->author->name }}">
                             <div>
-                                <a href="#" rel="author"
+                                <a href="/posts?author={{ $post->author->username }}" rel="author"
                                     class="text-xl font-bold text-gray-900 dark:text-white">{{ $post->author->name }}
                                 </a>
                                 <p class="text-base text-gray-500 dark:text-gray-400 mb-1">
                                     {{ $post['created_at']->diffForHumans() }}
                                 </p>
-                                <a href="/categories/{{ $post->category->slug }}">
+                                <a href="/posts?category={{ $post->category->slug }}">
                                     <span
                                         class="bg-{{ $post->category->color }}-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
                                         {{ $post->category->name }}
@@ -54,5 +37,4 @@
             </article>
         </div>
     </main>
-
 </x-layout>

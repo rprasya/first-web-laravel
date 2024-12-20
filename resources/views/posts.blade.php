a221f576a6f4510c25e372e443df36b6e6b26fec
Rinaldi Prasya <103562307+rprasya@users.noreply.github.com>
2024-12-20 13:36:16 +0700

    feat: modify blog page to use pagination

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -36,7 +36,7 @@
         </div>
     </div>
 
-    <div class="py-4 px-4 mx-auto max-w-screen-xl lg:py-8 lg:px-0">
+    <div class="py-4 my-4 px-4 mx-auto max-w-screen-xl lg:py-8 lg:px-0">
         <div class="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
             @forelse ($posts as $post)
                 <article
@@ -87,4 +87,6 @@
             @endforelse
         </div>
     </div>
+
+    {{ $posts->links() }}
 </x-layout>
